de5a752f7f83cf94f941c64955c95215dd8186d0
daxida <galmaw@hotmail.com>
2024-05-28 20:58:10 +0200

    Trying to fix the parentheses layout

--- src/stub/preprocessor/lisp_like.rs
+++ src/stub/preprocessor/lisp_like.rs
@@ -70,14 +70,17 @@ struct ReadBatch {
 
 impl Renderable for ReadBatch {
     fn render(&self, renderer: &crate::stub::renderer::Renderer) -> String {
+        // Swap line_readers and nested_cmds order
         let nested_string: String =
             self.nested_cmds.iter().map(|cmd| renderer.render_command(cmd, 0)).collect();
-        let nested_lines: Vec<&str> = nested_string.lines().collect();
+        let nested_lines: Vec<&str> = nested_string.lines().filter(|ln| !ln.is_empty()).collect();
 
         let read_lines: String =
             self.line_readers.iter().map(|cmd| renderer.render_command(cmd, 0)).collect();
-        let read_lines: Vec<&str> = read_lines.lines().collect();
+        let read_lines: Vec<&str> = read_lines.lines().filter(|ln| !ln.is_empty()).collect();
 
+        // print!("{:?}\n\n", read_lines);
+        print!("{:?}\n\n", nested_lines);
         let mut context = tera::Context::new();
         context.insert("read_lines", &read_lines);
         context.insert("nested_lines", &nested_lines);
